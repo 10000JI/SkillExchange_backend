8b99482a7e224505b7d78fdfb84d97ef3bd76332
10000JI <sksk436@naver.com>
2024-05-21 19:39:53 +0900

    temporary

--- src/main/java/place/skillexchange/backend/WebRestController.java
+++ src/main/java/place/skillexchange/backend/WebRestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +14,6 @@ public class WebRestController {
 
     @GetMapping("/profile")
     public String getProfile(){
-        return Arrays.stream(env.getActiveProfiles())
-                .collect(Collectors.joining(","));
+        return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
 }
