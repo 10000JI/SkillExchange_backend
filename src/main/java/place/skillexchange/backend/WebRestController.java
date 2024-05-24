750dd331d66257a402ea541ea5346c0b873ce69e
10000JI <sksk436@naver.com>
2024-05-24 14:05:04 +0900

    temporary

--- src/main/java/place/skillexchange/backend/WebRestController.java
+++ src/main/java/place/skillexchange/backend/WebRestController.java
@@ -17,6 +17,6 @@ public class WebRestController {
         return Arrays.stream(env.getActiveProfiles())
                 .skip(1) // 첫번째 profile 스킵
                 .findFirst() // 이후 나온 profile 적용 (ex> set1, set2)
-                .orElse("");
+                .orElse(""); // pollscm 테스트용 주석
     }
 }
