a7b2005e7d640aed5c677545e01ace91b0676d70
10000JI <sksk436@naver.com>
2024-05-25 02:48:47 +0900

    temporary

--- src/main/java/place/skillexchange/backend/WebRestController.java
+++ src/main/java/place/skillexchange/backend/WebRestController.java
@@ -13,10 +13,7 @@ public class WebRestController {
     private final Environment env;
 
     @GetMapping("/profile")
-    public String getProfile() {
-        return Arrays.stream(env.getActiveProfiles())
-                .skip(1) // 첫번째 profile 스킵
-                .findFirst() // 이후 나온 profile 적용 (ex> set1, set2)
-                .orElse(""); // pollscm 테스트용 주석
+    public String getProfile(){
+        return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
 }
