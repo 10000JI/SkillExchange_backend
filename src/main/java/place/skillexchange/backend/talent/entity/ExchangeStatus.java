6be1e1c8ef4b7c0d33848befe7f222622c933587
10000JI <sksk436@naver.com>
2024-07-02 18:36:01 +0900

    재능교환 매칭 : ExchangeStatus의 COMPLETED 없앰 -> 매칭되면 재능교환 게시물 삭제로 변경

--- src/main/java/place/skillexchange/backend/talent/entity/ExchangeStatus.java
+++ src/main/java/place/skillexchange/backend/talent/entity/ExchangeStatus.java
@@ -2,6 +2,5 @@ package place.skillexchange.backend.talent.entity;
 
 public enum ExchangeStatus {
     PENDING,    // 재능교환 요청 전
-    REQUESTED,  // 재능교환 요청됨
-    COMPLETED   // 재능교환 요청 완료
+    REQUESTED  // 재능교환 요청됨
 }
